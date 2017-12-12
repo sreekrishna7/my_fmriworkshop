f7e29d14f8e3dbaa4b5fc9238f7a452906ca7d6d
Sreekrishna Ramakrishnapillai <sreekrishna7@gmail.com>
2017-12-12 10:27:40 -0600

    Adding prepro.py

---
+++ prepro.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 12 09:56:59 2017
+
+@author: sreekrishna
+"""
+
+def prepro(basedir):
+    print('Hello data in path '+basedir)
+ 
+def main():
+    basedir='/home/sreekrishna/ds000030_R1.0.5'
+    prepro(basedir)
+
+main()
